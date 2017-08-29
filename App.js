37908a9553231006de7386feb08e7244847af40f
Christian Pena <christianpena9@gmail.com>
2017-08-28 22:49:15 -0400

    added a new component for route and also added a simple route navigation using state

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
 import React, {Component} from 'react';
 import {Text, View, Platform, Image} from 'react-native';
 import Landing from './src/Landing';
+import Search from './src/Search';
 
 export default class ReactNativePokeSearch extends Component {
+    constructor(props,context) {
+        super(props, context);
+        this.state = {
+            currentScreen: 'landing'
+        }
+    }
+    
+    switchScreen = (currentScreen) => {
+        this.setState({currentScreen});
+    }
+    
+    renderScreen = () => {
+        if(this.state.currentScreen === 'landing') {
+            return (
+                <Landing switchScreen={this.switchScreen}/>
+            );
+        } else if (this.state.currentScreen === 'search') {
+            return (
+                <Search />
+            );
+        }
+    } // End of renderScreen
+    
     render() {
         return (
             <View style={styles.container}>
-                <Landing />
+                {this.renderScreen()}
             </View>
         );
     }
