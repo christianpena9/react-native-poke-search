a3c770ff57aa397ae84218d8a8da04753777e300
Christian Pena <christianpena9@gmail.com>
2017-08-28 22:15:01 -0400

    added background image and button on the homepage of the app

--- App.js
+++ App.js
@@ -1,45 +1,59 @@
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, {Component} from 'react';
+import {Text, View, Platform, Image, Dimensions} from 'react-native';
+import {Button} from 'native-base';
+
+var width = Dimensions.get('window').width; //full width
+var height = Dimensions.get('window').height; //full height
+var myBackground = require('./assets/landing.jpg');
 
 export default class ReactNativePokeSearch extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Image style={styles.image} source={myBackground}>
+                    <View style={styles.viewStyle}>
+                        <Text style={styles.titleStyle}>Welcome to PokeSearch</Text>
+                        <Button
+                            block={true}
+                            style={styles.buttonStyle}
+                            onPress={() =>{}}
+                        >
+                            <Text style={styles.buttonText}>Start Searching</Text>
+                        </Button>
+                    </View>
+                </Image>
+            </View>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+const styles = {
+    container: {
+        flex: 1,
+        // alignItems: 'stretch'
+        //marginTop: Platform.OS === 'android' ? 24 : 0
+    },
+    image: {
+        height: height,
+        width: width,
+        marginTop: -25,
+    },
+    viewStyle: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    titleStyle: {
+        fontSize: 30,
+        color: 'blue',
+        alignItems: 'center',
+    },
+    buttonStyle: {
+        margin: 10
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 30,
+    }
+};
